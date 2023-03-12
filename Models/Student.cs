6efe485e480930c7c7aa995fbb39549ad8413b7d
Cornelius Fillmore <palubickimaciej5@gmail.com>
2023-03-12 13:19:46 +0100

    Updating README.MD

--- Models/Student.cs
+++ Models/Student.cs
@@ -4,5 +4,8 @@
     {
         public string Name { get; set; }
         public string Surname { get; set; }
+        public int PhoneNumber { get; set; }
+        public string PhoneName { get; set; }
+
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # PrivateLessonsPlannerApi
+
+---
+
+## Description
+
+An Api used for the web app used to plan, organize and edit PrivateLessons
+
